6f27922a3fed7c88e719012a0ed8383c05dc6311
DaviZ1986 <125762695+DaviZ1986@users.noreply.github.com>
2023-03-03 22:28:21 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,5 +1,8 @@
 # python3
 
+#Dāvis Zommers 221RDB150 7.grupa
+
+from itertools import combinations_with_replacement
 import sys
 import threading
 import numpy
@@ -9,12 +12,49 @@ def compute_height(n, parents):
     # Write this function
     max_height = 0
     # Your code here
-    return max_height
+    for i in range (len(parents)):
+        if (parents[i] == n):
+            childHeight = compute_height(i, parents)
+            if (childHeight > max_height):
+                max_height = childHeight
+    return max_height + 1
 
 
 def main():
     # implement input form keyboard and from files
-    
+
+    choice = input()
+    n = int(input())
+    if ("I" in choice):
+        #n = int(input())
+        elements = input().split()
+    #D:/testfails.txt
+    if ("F" in choice):
+        print("Input file path")
+        while True:
+            path = input()
+            if ("a" in path.lower()):
+                print("File not allowed")
+            else:
+                f = open(path, "r")
+                elements = f.read().split()
+                f.close()
+                break
+
+    parents = []
+    try:
+        for i in range(0, n):
+            if (-1 <= int(elements[i]) and int(elements[i]) <= n-1):
+                parents.append(int(elements[i]))
+            else:
+                break
+
+        root = parents.index(-1)
+
+        height = compute_height(root, parents)
+        print(height)
+    except:
+        print("error")
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     
